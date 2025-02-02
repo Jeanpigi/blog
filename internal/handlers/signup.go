c6adf6b2601f28a4ae27c6e40d3d4d621c09d58d
Jeanpigi <stick-master666@hotmail.com>
2025-02-02 13:25:16 -0500

    mejorando cositas sec

--- internal/handlers/signup.go
+++ internal/handlers/signup.go
@@ -7,32 +7,57 @@ import (
 	"github.com/Jeanpigi/blog/db"
 	"github.com/Jeanpigi/blog/internal/models"
 	"github.com/Jeanpigi/blog/internal/utils"
+	"github.com/Jeanpigi/blog/session"
 )
 
+// SignupHandler protege la ruta para que solo usuarios autenticados puedan acceder
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	// Verificar si el usuario está autenticado
+	session, err := session.Store.Get(r, "session-name")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	username, ok := session.Values["username"].(string)
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	user, _ := db.GetUserByUsername(username)
+	if user == nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	// Si la petición es GET, renderizar el formulario de registro
 	if r.Method == "GET" {
 		utils.RenderTemplate(w, "templates/signup.html", nil)
-	} else if r.Method == "POST" {
-		// Procesar el formulario de registro
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		// Crear un nuevo usuario
-		user := models.User{
-			Username: username,
-			Password: utils.HashPassword(password),
-		}
-
-		// Guardar el usuario en la base de datos (implementación propia)
-		err := db.InsertUser(&user)
-		if err != nil {
-			log.Println("Error al insertar el usuario en la base de datos:", err)
-			// Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
-			return
-		}
-
-		// Iniciar sesión y redirigir al dashboard
-		utils.StartSession(w, r, username)
-		http.Redirect(w, r, "/dashboard", http.StatusFound)
+		return
 	}
+
+	// Si la petición es POST, procesar el formulario de registro
+	username = r.FormValue("username")
+	password := r.FormValue("password")
+
+	// Crear un nuevo usuario
+	newUser := models.User{
+		Username: username,
+		Password: utils.HashPassword(password),
+	}
+
+	// Guardar el usuario en la base de datos
+	err = db.InsertUser(&newUser)
+	if err != nil {
+		log.Println("Error al insertar el usuario en la base de datos:", err)
+		// Manejar error
+		return
+	}
+
+	// Iniciar sesión automáticamente después del registro
+	utils.StartSession(w, r, username)
+
+	// Redirigir al dashboard
+	http.Redirect(w, r, "/dashboard", http.StatusFound)
 }
